45a524f578fc58b23cc38aa00bdf0747fc320c7a
jefenso <jaespiritu@student.apc.edu.ph>
2023-03-05 20:06:25 +0800

    echo innit

--- ci4/app/Views/templates/navbar.php
+++ ci4/app/Views/templates/navbar.php
@@ -1,6 +1,6 @@
 <div class="w3-row w3-padding w3-black w3-opacity-min">
       <div class="w3-col s3">
-        <a href="home" class="w3-button w3-block w3-black glow2 w3-hover-purple">HOME</a>
+        <a href="<?php echo base_url() ?>/home" class="w3-button w3-block w3-black glow2 w3-hover-purple">HOME</a>
       </div>
       <div class="w3-col s3">
         <a href="INTERESTS" class="w3-button w3-block w3-black glow2 w3-hover-purple">INTERESTS</a>
@@ -9,7 +9,7 @@
         <a href="HOBBIES" class="w3-button w3-block w3-black glow2 w3-hover-purple">HOBBIES</a>
       </div>
       <div class="w3-col s3">
-        <a href="GALLERY" class="w3-button w3-block w3-black glow2 w3-hover-purple">IMAGES</a>
+        <a href="<?php echo base_url() ?>/GALLERY" class="w3-button w3-block w3-black glow2 w3-hover-purple">IMAGES</a>
       </div>
     </div>
   </div>
