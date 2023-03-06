e44432910fa1db80b548b8dcc364da78a57f5139
jefenso <jaespiritu@student.apc.edu.ph>
2023-03-06 20:04:44 +0800

    guest or news fix

--- ci4/app/Views/templates/navbar.php
+++ ci4/app/Views/templates/navbar.php
@@ -6,7 +6,7 @@
         <a href="<?php echo base_url() ?>/INTERESTS" class="w3-button w3-block w3-black glow2 w3-hover-purple">INTERESTS</a>
       </div>
       <div class="w3-col s3">
-        <a href="<?php echo base_url() ?>/news" class="w3-button w3-block w3-black glow2 w3-hover-purple">NEWS</a>
+        <a href="<?php echo base_url() ?>/news" class="w3-button w3-block w3-black glow2 w3-hover-purple">NEWS/GUESTS</a>
       </div>
       <div class="w3-col s3">
         <a href="<?php echo base_url() ?>/GALLERY" class="w3-button w3-block w3-black glow2 w3-hover-purple">IMAGES</a>
